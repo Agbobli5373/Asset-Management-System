006df07d06c4bee5eba443554627186a7a859147
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-03-12 16:02:03 +0000

    add Category functionality

--- admin/addCategory.php
+++ admin/addCategory.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="en">
-<?php $title = "Add User" ?>
+<?php $title = "Add Asset Category" ?>
 <?php require_once '../share/head.php'; ?>
 
 <body data-sidebar="dark">
@@ -23,9 +23,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-flex align-items-center justify-content-between">
-                                <h4 class="mb-0 font-size-18">Categorys</h4>
-
-
+                                <h4 class="mb-0 font-size-18">Asset Categories</h4>
 
                             </div>
                         </div>
