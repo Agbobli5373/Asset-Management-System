9ea7abc2e15500a7aabc612b2cc0ebf023369fa1
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-03-12 17:28:16 +0000

    loading category and department for creating asset

---
+++ admin/addAssert.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+<?php $title = "Add Asset Category" ?>
+<?php require_once '../share/head.php'; ?>
+<?php require_once('../config/dbConnect.php') ?>
+
+<body data-sidebar="dark">
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        <?php require_once '../share/sidebar.php';  ?>
+
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0 font-size-18">Asset Categories</h4>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row justify-content-center">
+                        <div class="col-sm-12 col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="header-title">Add New Category</h4>
+
+
+                                    <form class="mt-4" id="add-student" method="POST">
+                                        <div class="row">
+                                            <div class="col-md-6 mb-3">
+                                                <label for="name">Name of the Asset</label>
+                                                <input type="text" name="name" class="form-control name" id="name" placeholder="Asset Name" required>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 mb-3">
+                                                <label for="department">Department</label>
+                                                <select class="form-control" id="department" name="department">
+
+                                                    <?php
+
+                                                    $get_departments = mysqli_query($connectionString, "SELECT * FROM department_tbl") or die(mysqli_error($connectionString));
+
+                                                    while ($each_department = mysqli_fetch_array($get_departments)) {
+                                                        $department_id = $each_department['department_id'];
+                                                        $department_name = $each_department['department_name'];
+
+                                                    ?>
+
+                                                        <option value="<?php echo $department_id; ?>"><?php echo $department_name;  ?></option>
+
+
+                                                    <?php  }    ?>
+
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 mb-3">
+                                                <label for="category">Category</label>
+                                                <select class="form-control" id="category" name="category">
+
+                                                    <?php
+
+                                                    $get_categorys = mysqli_query($connectionString, "SELECT * FROM category_tbl") or die(mysqli_error($connectionString));
+
+                                                    while ($each_category = mysqli_fetch_array($get_categorys)) {
+                                                        $category_id = $each_category['category_id'];
+                                                        $category_name = $each_category['category_name'];
+
+                                                    ?>
+
+                                                        <option value="<?php echo $category_id; ?>"><?php echo $category_name;  ?></option>
+
+
+                                                    <?php  }    ?>
+
+
+                                                </select>
+                                            </div>
+
+                                            <div class="col-md-6 mb-3">
+                                                <label for="depreciate">Depreciate Percentage</label>
+                                                <input type="number" name="depreciate" class="form-control depreciate" id="depreciate" placeholder="Depreciate" required>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 mb-3">
+                                                <label for="remark">Remark</label>
+                                                <input type="text" name="remark" class="form-control phone" id="remark" placeholder="Remark" required>
+                                                <div class="valid-feedback">
+                                                    Looks good!
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button class="btn btn-primary text-center" type="submit">Add Category</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
+
+
+
+                    <?php require_once '../share/footer.php'; ?>
+
+                </div>
+                <!-- end main content-->
+
+            </div>
+            <!-- END layout-wrapper -->
+
+
+
+            <!-- Right bar overlay-->
+            <div class="rightbar-overlay"></div>
+
+            <?php require_once("../share/script.php") ?>
+
+            <!--
+            <script type='text/javascript'>
+                $(document).ready(function() {
+
+                    alertify.set('notifier', 'position', 'top-right');
+                    $('#add-category').submit(function(e) {
+                        e.preventDefault();
+                        var formdata = $(this).serialize();
+                        if ($('.category-name').val() === '') {
+                            alertify.error("Please Enter Name");
+                        } else {
+
+                            $.ajax({
+                                url: '../api_calls/add-category.php',
+                                type: 'POST',
+                                data: formdata,
+                                success: function(res) {
+                                    if (res.trim() === "success") {
+                                        $('.category-name').val('');
+                                        alertify.success("Added Successfully");
+
+                                    } else if (res.trim() === "already") {
+                                        alertify.error("Name Exists Already");
+
+                                    }
+
+                                },
+                                error: function(res) {
+                                    console.log(res);
+                                }
+
+                            });
+
+                        }
+
+                    })
+
+
+
+                }); -->
+            </script>
+
+
+</body>
+
+
+</html>
